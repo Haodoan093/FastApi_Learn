ac5de42c8fef0002af86ad6214e5ef0b38bec9f4
Haodoan093 <122719130+Haodoan093@users.noreply.github.com>
2024-12-15 23:41:16 +0700

    Learn FastAPI

--- main.py
+++ main.py
@@ -1,54 +1,62 @@
 from http.client import HTTPException
+from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
 
-class Item(BaseModel):
+class Student(BaseModel):
     name: str
-    price: float
-    is_offer: bool = None
+    age: int
+    year: str
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
 
 
+students = {
+    1: Student(name="John", age=21, year="2021"),
+    2: Student(name="Jane", age=23, year="2021"),
+}
 
-items_db = []
+@app.get("/")
+def index():
+    return students
+@app.get("/students/{student_id}")
+def get_student(student_id: int = Path(..., description="The ID of the student to get")):
+    return students[student_id]
 
-
-
-@app.post("/items/")
-def create_item(item: Item):
-    items_db.append(item)
-    return {"name": item.name, "price": item.price, "is_offer": item.is_offer}
-
-
-
-@app.get("/items/")
-def get_items():
-    return items_db
-
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-
-    if item_id >= len(items_db) or item_id < 0:
-        raise HTTPException(status_code=404, detail="Item not found")
+@app.get("/get-by-name/{student_id}")
+def get_student(*, student_id: int, name: Optional[str]= None, test : int):
+    for student_id in students:
+        if students[student_id].name == name:
+            return students[student_id]
+    return {"data": "No student found"}
 
 
-    items_db[item_id] = item
-    return {"name": item.name, "price": item.price, "is_offer": item.is_offer}
+@app.post("/students/{student_id}")
+def create_student(student_id: int,student: Student):
+    if student_id in students:
+        return {"Error":"Student with this ID already exists"}
+    students[student_id] = student
+    return student
 
+@app.put("/students/{student_id}")
+def update_student(student_id: int,student: UpdateStudent):
+    if student_id not in students:
+        return {"Error":"Student with this ID does not exist"}
+    for key, value in student.dict().items():
+        if value is not None:
+            setattr(students[student_id], key, value)
+    return students[student_id]
 
-
-@app.delete("/items/{item_id}")
-def delete_item(item_id: int):
-
-    if item_id >= len(items_db) or item_id < 0:
-        raise HTTPException(status_code=404, detail="Item not found")
-
-
-    deleted_item = items_db.pop(item_id)
-    return {"message": "Item deleted", "deleted_item": deleted_item}
+@app.delete("/students/{student_id}")
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {"Error":"Student with this ID does not exist"}
+    del students[student_id]
+    return {"message": "Student deleted successfully"}
